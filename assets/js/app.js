28c030a345aa04530541b1d4b575d137c794d9f2
relexo <wj-8530@163.com>
2019-08-29 09:46:38 +0800

    第三次提交

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-﻿var queryParamWorksId = getQueryString("wordsId");
+﻿var queryParamWorksId = getQueryString("worksId");
 var queryParamWorksNum = getQueryString("worksNum");
 var queryParamToken = getQueryString("token");
 
@@ -132,108 +132,13 @@ function InitPage(loadWindowFunc) {
             threeHelper.action.push(mixer.clipAction(object.animations[0]));
             threeHelper.action[0].play();
         }
-        LoadObjAnimation(threeHelper, object, loadWindowFunc);
+        setTimeout(function () {
+            LoadObjAnimation(threeHelper, object, loadWindowFunc);
+        }, 2000);
+
 
     });
 
-    function LoadObjAnimation(threeHelperObj, object, callback) {
-        $.ajax({
-            url: "https://test.cdzghome.com:3176/national/works/getWorksInfoById?id=1157554665578332268",
-            type: "Post",
-            //data: { id: 1157554665578332268 },
-            dataType: "Json",
-            success: function (res) {
-                if (res.code == 200) {
-                    object.traverse(function (child) {
-                        for (var i = 0; i < child.children.length; i++) {
-                            if (child.children[i].name == "IMG") {
-                                //textureLoader.setCrossOrigin('*');
-                                //var texture = textureLoader.load(res.data.medias[0].mlink);
-                                var texture = textureLoader.load("assets/images/3DModel/img.png");
-                                var material = new THREE.MeshStandardMaterial({
-                                    map: texture,
-                                    transparent: true
-                                });
-                                child.children[i].material = material;
-                            }
-                        }
-                    });
-
-
-
-                    //之后请求抽字接口
-                    $.ajax({
-                        url: "https://test.cdzghome.com:3176/singleWord/gatherOneWord",
-                        type: "Post",
-                        dataType: "Json",
-                        data: JSON.stringify({ latitude: 34.3619400000, longitude: 107.2373200000, workIdAndQrCode: "1157554665578332268-1" }),
-                        dataType: "Json",
-                        headers: { 'Content-Type': 'application/json;charset=UTF-8', token: "a62bf553-cb1f-4f39-83ca-b8c0cfe5eb1a" },
-                        success: function (res) {
-                            const settingAnimation = {
-                                model: "assets/images/3DModel/70zn/70zn_hb.fbx",
-                                scale: 0.5,
-                                position: [8, 30, 150]
-                            };
-                            var animationTextures = [];
-                            //模型加载部分
-                            var animationLoader = new THREE.FBXLoader();
-                            animationTextures.push(textureLoader.load('assets/images/3DModel/70zn/hb_1.png'));
-                            animationTextures.push(textureLoader.load('assets/images/3DModel/70zn/hb_2.png'));
-                            animationLoader.load(settingAnimation.model, (object) => {
-                                object.scale.multiplyScalar(settingAnimation.scale);
-                                object.position.set(settingAnimation.position[0], settingAnimation.position[1], settingAnimation.position[2]);
-                                object.traverse(function (child) {
-                                    for (var i = 0; i < child.children.length; i++) {
-                                        if (child.children[i].name == "Sphere001") {
-                                            console.info(child.children[i].name)
-                                            child.children[i].visible = false;
-                                        }
-                                        console.info(child.children[i].name);
-                                        if (child.children[i].isMesh) {
-                                            var material = new THREE.MeshStandardMaterial({
-                                                transparent: true
-                                            });
-                                            switch (child.children[i].name) {
-                                                case "Plane014":
-                                                    material.map = animationTextures[0];
-                                                    child.children[i].material = material;
-                                                    break;
-                                                case "Plane015":
-                                                    material.map = animationTextures[1];
-                                                    child.children[i].material = material;
-                                                    break;
-                                                default:
-                                            }
-                                        }
-                                    }
-                                });
-                                //模型加载到场景里之后，开始请求接口
-                                threeHelperObj.scene.add(object);
-                                var action;
-                                if (object.animations.length > 0) {
-                                    var mixer = new THREE.AnimationMixer(object);
-                                    threeHelperObj.mixers.push(mixer);
-                                    action = mixer.clipAction(object.animations[0]);
-                                    action.play();
-                                }
-                                setTimeout(function () {
-                                    for (var i = 0; i < threeHelperObj.mixers.length; i++) {
-                                        if (i == 1) {
-                                            threeHelperObj.mixers.splice(i, 1);
-                                        }
-                                    }
-                                    object.visible = false;
-                                    callback(JSON.stringify(res));//该回调直接返回中奖结果
-                                }, 1580);
-                            });
-                        }
-                    });
-                }
-
-            }
-        });
-    }
     function setwebcam() {
         navigator.mediaDevices.enumerateDevices().then(function (devices) {
             devices.forEach(device => {
@@ -256,8 +161,42 @@ function InitPage(loadWindowFunc) {
             });
     }
 
-    //定位服务
-    function getCurrentPosition() {//调用浏览器定位服务
+    function LoadObjAnimation(threeHelperObj, object, callback) {
+        $.ajax({
+            url: "https://test.cdzghome.com:3176/national/works/getWorksInfoById?id=" + queryParamWorksId,
+            type: "Post",
+            dataType: "Json",
+            success: function (res) {
+                console.info(JSON.stringify(res));
+                if (res.code == 200) {
+                    object.traverse(function (child) {
+                        for (var i = 0; i < child.children.length; i++) {
+                            if (child.children[i].name == "IMG") {
+                                textureLoader.setCrossOrigin('*');
+                                //var texture = textureLoader.load(res.data.medias[0].mlink);
+                                var texture = textureLoader.load("assets/images/3DModel/img.png");
+                                var material = new THREE.MeshStandardMaterial({
+                                    map: texture,
+                                    transparent: true
+                                });
+                                child.children[i].material = material;
+                            }
+                        }
+                    });
+                    //先通过地图加载经纬度，后请求抽字接口
+                    //loadLngLat(threeHelperObj, callback);
+                }
+            }
+        });
+    }
+
+    function loadLngLat(threeHelperObj, callback) {
+        var body = document.getElementsByTagName("body")[0];
+        var container = document.createElement("div");
+        container.setAttribute("id", "container");
+        container.style.display = "none";
+        body.appendChild(container);
+        const map = new AMap.Map('container');
         map.plugin('AMap.Geolocation', function () {
             geolocation = new AMap.Geolocation({
                 enableHighAccuracy: true, //是否使用高精度定位，默认:true
@@ -268,34 +207,84 @@ function InitPage(loadWindowFunc) {
             });
             map.addControl(geolocation);
             geolocation.getCurrentPosition();
-            AMap.event.addListener(geolocation, 'complete', onComplete);
+            AMap.event.addListener(geolocation, 'complete', function (data) {
+                requestWord(threeHelperObj, data.position.getLng(), data.position.getLat(), callback);
+            });
             //返回定位信息
-            AMap.event.addListener(geolocation, 'error', onError);
+            AMap.event.addListener(geolocation, 'error', function (data) {
+                requestWord(threeHelperObj, 0, 0, callback);
+            });
             //返回定位出错信息
         });
-    };
+    }
 
-    //获取经纬度结果数组，0为经度，1为纬度
-    function onComplete(data) {
-        return [data.position.getLng(), data.position.getLat()];
-    };
-    //获取错误信息，返回错误结果提示
-    function onError(data) {
-        var str;
-        switch (data.info) {
-            case 'PERMISSION_DENIED':
-                str += '浏览器阻止了定位操作';
-                break;
-            case 'POSITION_UNAVAILBLE':
-                str += '无法获得当前位置';
-                break;
-            case 'TIMEOUT':
-                str += '定位超时';
-                break;
-            default:
-                str += '未知错误';
-                break;
-        }
-        return str;
-    };
+    function requestWord(threeHelperObj, lng, lat, callback) {
+        $.ajax({
+            url: "https://test.cdzghome.com:3176/singleWord/gatherOneWord",
+            type: "Post",
+            dataType: "Json",
+            data: JSON.stringify({ latitude: lng, longitude: lat, workIdAndQrCode: queryParamWorksId + "-" + queryParamWorksNum }),
+            dataType: "Json",
+            headers: { 'Content-Type': 'application/json;charset=UTF-8', token: queryParamToken },
+            success: function (res) {
+                const settingAnimation = {
+                    model: "assets/images/3DModel/70zn/70zn_hb.fbx",
+                    scale: 0.5,
+                    position: [8, 30, 150]
+                };
+                var animationTextures = [];
+                //模型加载部分
+                //var animationLoader = new THREE.FBXLoader();
+                animationTextures.push(textureLoader.load('assets/images/3DModel/70zn/hb_1.png'));
+                animationTextures.push(textureLoader.load('assets/images/3DModel/70zn/hb_2.png'));
+                loader.load(settingAnimation.model, (object) => {
+                    object.scale.multiplyScalar(settingAnimation.scale);
+                    object.position.set(settingAnimation.position[0], settingAnimation.position[1], settingAnimation.position[2]);
+                    object.traverse(function (child) {
+                        for (var i = 0; i < child.children.length; i++) {
+                            if (child.children[i].name == "Sphere001") {
+                                console.info(child.children[i].name)
+                                child.children[i].visible = false;
+                            }
+                            console.info(child.children[i].name);
+                            if (child.children[i].isMesh) {
+                                var material = new THREE.MeshStandardMaterial({
+                                    transparent: true
+                                });
+                                switch (child.children[i].name) {
+                                    case "Plane014":
+                                        material.map = animationTextures[0];
+                                        child.children[i].material = material;
+                                        break;
+                                    case "Plane015":
+                                        material.map = animationTextures[1];
+                                        child.children[i].material = material;
+                                        break;
+                                    default:
+                                }
+                            }
+                        }
+                    });
+                    //模型加载到场景里之后，开始请求接口
+                    threeHelperObj.scene.add(object);
+                    var action;
+                    if (object.animations.length > 0) {
+                        var mixer = new THREE.AnimationMixer(object);
+                        threeHelperObj.mixers.push(mixer);
+                        action = mixer.clipAction(object.animations[0]);
+                        action.play();
+                    }
+                    setTimeout(function () {
+                        for (var i = 0; i < threeHelperObj.mixers.length; i++) {
+                            if (i == 1) {
+                                threeHelperObj.mixers.splice(i, 1);
+                            }
+                        }
+                        object.visible = false;
+                        callback(JSON.stringify(res));//该回调直接返回中奖结果
+                    }, 1580);
+                });
+            }
+        });
+    }
 }
